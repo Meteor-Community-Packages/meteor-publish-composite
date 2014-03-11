a4884db00ac6ad5506ce224bbda2485072997dd6
Sean Dwyer <sean.dwyer@gmail.com>
2014-03-11 13:17:18 -0700

    Tests

--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 Posts = new Meteor.Collection('posts');
 Authors = new Meteor.Collection('authors');
+Comments = new Meteor.Collection('comments');
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
@@ -13,8 +14,20 @@ if (Meteor.isServer) {
         children: [
             {
                 find: function(post) {
-                    return Authors.find({ _id: post.authorId }, { fields: { profile: 1 } });
+                    return Authors.find({ _id: post.authorId });
                 }
+            },
+            {
+                find: function(post) {
+                    return Comments.find({ postId: post._id });
+                },
+                children: [
+                    {
+                        find: function(comment) {
+                            return Authors.find({ _id: comment.authorId });
+                        }
+                    }
+                ]
             }
         ]
     });
@@ -40,10 +53,13 @@ if (Meteor.isClient) {
     testAsyncMulti("Should publish all posts and authors", [function(test, expect) {
         var subscription = Meteor.subscribe('allPosts', expect(function() {
             var posts = Posts.find();
-            test.equal(posts.count(), 4);
+            test.equal(posts.count(), 3, "Post count");
 
             var authors = Authors.find();
-            test.equal(authors.count(), 3);
+            test.equal(authors.count(), 4, "Author count");
+
+            var comments = Comments.find();
+            test.equal(comments.count(), 4, "Comment count");
 
             subscription.stop();
         }));
@@ -52,10 +68,10 @@ if (Meteor.isClient) {
     testAsyncMulti("Should publish one user's posts", [function(test, expect) {
         var subscription = Meteor.subscribe('userPosts', 'marie', expect(function() {
             var posts = Posts.find();
-            test.equal(posts.count(), 2);
+            test.equal(posts.count(), 2, "Post count");
 
             var authors = Authors.find();
-            test.equal(authors.count(), 1);
+            test.equal(authors.count(), 1, "Author count");
 
             subscription.stop();
         }));
@@ -64,62 +80,74 @@ if (Meteor.isClient) {
 
 
 // Util functions
-var initData = function() {
-    initUsers();
-    initPosts();
-};
+var initData = (function() {
+    return function() {
+        removeAllData();
+        initUsers();
+        initPosts();
+    };
 
-var initUsers = function() {
-    if (Authors.find().count() <= 0) {
-        console.log('no users found, adding some');
-        Authors.insert({
-            username: 'marie',
-            password: '123456',
-            profile: {
-                name: 'Marie Curie'
-            }
-        });
-        Authors.insert({
-            username: 'albert',
-            password: '123456',
-            profile: {
-                name: 'Albert Einstein'
-            }
-        });
-        Authors.insert({
-            username: 'richard',
-            password: '123456',
-            profile: {
-                name: 'Richard Feynman'
-            }
-        });
+    function removeAllData() {
+        Comments.remove();
+        Posts.remove();
+        Authors.remove();
     }
-};
 
-var initPosts = function() {
-    if (Posts.find().count() <= 0) {
-        var marie = Authors.findOne({ username: 'marie' });
-        var albert = Authors.findOne({ username: 'albert' });
-        var richard = Authors.findOne({ username: 'richard' });
+    function initUsers() {
+        if (Authors.find().count() <= 0) {
+            console.log('no users found, adding some');
 
-        Posts.insert({
-            title: 'Marie\'s first post',
-            authorId: marie._id
+            Authors.insert({ username: 'marie' });
+            Authors.insert({ username: 'albert' });
+            Authors.insert({ username: 'richard' });
+            Authors.insert({ username: 'stephen' });
+            Authors.insert({ username: 'john' });
+        }
+    };
+
+    function initPosts() {
+        if (Posts.find().count() <= 0) {
+            console.log('no posts found, adding some');
+
+            var marie = Authors.findOne({ username: 'marie' });
+            var albert = Authors.findOne({ username: 'albert' });
+            var richard = Authors.findOne({ username: 'richard' });
+            var stephen = Authors.findOne({ username: 'stephen' });
+
+            insertPost('Marie\'s first post', marie._id, [{
+                text: "Comment text",
+                authorId: albert._id
+            }]);
+
+            insertPost('Marie\'s second post', marie._id, [
+                {
+                    text: "Comment one",
+                    authorId: richard._id
+                },
+                {
+                    text: "Comment two",
+                    authorId: stephen._id
+                }
+            ]);
+
+            insertPost('Albert\'s first post', albert._id, [{
+                text: "Comment text",
+                authorId: richard._id
+            }]);
+        }
+    };
+
+    function insertPost(title, authorId, comments) {
+        var postId = Posts.insert({
+            title: title,
+            authorId: authorId
         });
 
-        Posts.insert({
-            title: 'Marie\'s second post',
-            authorId: marie._id
-        });
+        var commentData;
+        for (var i = 0; i < comments.length; i++) {
+            commentData = _.extend({}, comments[i], { postId: postId });
 
-        Posts.insert({
-            title: 'Albert\'s first post',
-            authorId: albert._id
-        });
-
-        Posts.insert({
-            title: 'Richard\'s first post',
-            authorId: richard._id
-        });
-    }
-};
+            Comments.insert(commentData);
+        }
+    };
+}());
