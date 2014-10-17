902bfbb68e0ab92393f0a885212b7146fef7cc41
Sean Dwyer <sean.dwyer@gmail.com>
2014-10-17 10:54:33 -0700

    Bump version to 1.3.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "reywood:publish-composite",
     summary: "Publish a set of related documents from multiple collections with a reactive join",
-    version: "1.3.1",
+    version: "1.3.2",
     git: "https://github.com/englue/meteor-publish-composite.git"
 });
 
